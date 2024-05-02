0ffad2d58402c430eff5e0ee00a24935a9bb38ef
s.potapov <s.potapov@severnaya.ru>
2024-05-02 13:15:51 +0300

    добавляет посередине win

--- src/components/WinRating/WinRating.jsx
+++ src/components/WinRating/WinRating.jsx
@@ -1,9 +1,12 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { globalContext } from "../../App";
 import "./WinRating.css";
 
 function WinRating() {
+  const [isMobile, setIsMobile] = useState(false);
   const { numberQuestion } = useContext(globalContext);
+  const [styleTransform, setStyletransform] = useState(0);
+
   const numbers = [
     100, 200, 300, 400, 500, 1000, 2000, 5000, 10000, 25000, 50000, 100000,
     250000, 500000, 1000000,
@@ -14,20 +17,26 @@ function WinRating() {
     winArray.push(<li key={i}>{numbers[i]}</li>);
   }
 
+  useEffect(() => {
+    return window.innerWidth <= 500 ? setIsMobile(true) : null;
+  }, []);
+
   return (
-    <ul className="win-rating-list">
-      {winArray.map((winItem, index) => {
-        if (index === numberQuestion) {
-          return (
-            <li className="bck-gold" key={index}>
-              {winItem}
-            </li>
-          );
-        } else {
-          return <li key={index}>{winItem}</li>;
-        }
-      })}
-    </ul>
+    <div className="win-wrapper">
+      <ul className="win-rating-list">
+        {winArray.map((winItem, index) => {
+          if (index === numberQuestion) {
+            return (
+              <li className="bck-gold" key={index}>
+                {winItem}
+              </li>
+            );
+          } else {
+            return <li key={index}>{winItem}</li>;
+          }
+        })}
+      </ul>
+    </div>
   );
 }
 
